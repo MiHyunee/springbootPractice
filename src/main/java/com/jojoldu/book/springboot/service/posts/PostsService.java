0d4294baa42f2b456000c27d65138f14d11634f0
ParkMiHyun <mihyunpark24@gmail.com>
2020-03-27 17:00:11 +0900

    게시글수정화면 에러수정, 게시글삭제화면 추가

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -33,6 +33,14 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
@@ -46,4 +54,5 @@ public class PostsService {
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
 }
--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
@@ -1,10 +1,8 @@
 package com.jojoldu.book.springboot.web.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,8 +10,6 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity) {
